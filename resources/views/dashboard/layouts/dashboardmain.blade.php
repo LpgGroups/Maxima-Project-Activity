053644d6be274df6e3d178f7689bab52371dffa2
muhammadricoasmara04 <ricoasmara491@gmail.com>
2025-04-02 18:16:23 +0700

    update vite

--- resources/views/dashboard/layouts/dashboardmain.blade.php
+++ resources/views/dashboard/layouts/dashboardmain.blade.php
@@ -6,8 +6,6 @@
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta name="csrf-token" content="{{ csrf_token() }}">
-    <meta name="csrf-token" content="{{ csrf_token() }}">
-
     @vite('public/css/app.css')
     <link href="https://fonts.googleapis.com/css2?family=Poppins:wght@400;500;600&display=swap" rel="stylesheet">
     <link href="https://cdn.jsdelivr.net/npm/flowbite@3.1.2/dist/flowbite.min.css" rel="stylesheet" />
