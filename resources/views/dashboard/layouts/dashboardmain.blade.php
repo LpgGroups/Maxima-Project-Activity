18dc77bd122b5a4b45db0be9e57517a923b808ae
muhammadricoasmara04 <ricoasmara491@gmail.com>
2025-04-02 18:25:16 +0700

    vite clear

--- resources/views/dashboard/layouts/dashboardmain.blade.php
+++ resources/views/dashboard/layouts/dashboardmain.blade.php
@@ -6,7 +6,7 @@
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta name="csrf-token" content="{{ csrf_token() }}">
-    @vite('public/css/app.css')
+    @vite('resources/css/app.css')
     <link href="https://fonts.googleapis.com/css2?family=Poppins:wght@400;500;600&display=swap" rel="stylesheet">
     <link href="https://cdn.jsdelivr.net/npm/flowbite@3.1.2/dist/flowbite.min.css" rel="stylesheet" />
     <script src="https://cdn.jsdelivr.net/npm/flowbite@3.1.2/dist/flowbite.min.js"></script>
