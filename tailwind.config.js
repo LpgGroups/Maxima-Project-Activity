1d8086c19f605e2adf3c368d4e0de590ec1356fc
muhammadricoasmara04 <ricoasmara491@gmail.com>
2025-04-06 10:31:25 +0700

    update js bug

--- tailwind.config.js
+++ tailwind.config.js
@@ -7,6 +7,7 @@ export default {
         "./storage/framework/views/*.php",
         "./resources/**/*.blade.php",
         "./resources/**/*.js",
+        "./public/**/*.js",
         "./resources/**/*.vue",
         "./public/**/*.css",
     ],
