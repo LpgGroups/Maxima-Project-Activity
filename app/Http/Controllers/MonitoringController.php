644de45adb456c5b0b1413dfa6a245b7809f9259
muhammadricoasmara04 <ricoasmara496@gmail.com>
2025-07-01 21:16:18 +0700

    update monitoring

--- app/Http/Controllers/MonitoringController.php
+++ app/Http/Controllers/MonitoringController.php
@@ -26,10 +26,10 @@ class MonitoringController extends Controller
         // Ambil notifikasi dari semua admin (yang role-nya admin)
         $adminIds = User::where('role', 'admin')->pluck('id');
 
-        $rawNotifications = DatabaseNotification::whereIn('notifiable_id', $adminIds)
-            ->where('notifiable_type', User::class)
+        $rawNotifications = DatabaseNotification::where('notifiable_type', User::class)
             ->where('data->training_id', $id)
             ->get();
+
 
         // Deduplicate berdasarkan kombinasi unik
         $notifications = $rawNotifications->unique(function ($notif) {
@@ -40,7 +40,7 @@ class MonitoringController extends Controller
             'training' => $training,
             'notifications' => $notifications,
             'title' => 'Monitoring Pelatihan',
-        
+
         ]);
     }
 }
