3f5c43ae703cd9e76a62fffbe3f96251f961e956
muhammadricoasmara04 <ricoasmara491@gmail.com>
2025-07-04 10:57:04 +0700

    update monitoring managagement

--- resources/views/dashboard/layouts/sidebarM.blade.php
+++ resources/views/dashboard/layouts/sidebarM.blade.php
@@ -143,6 +143,20 @@
                             <span class="text-sm">Management Dashboard</span>
                         </a>
                     </li>
+
+                    <li class="my-1">
+                        <a href="/dashboard/monitoring"
+                            class="flex items-center space-x-2 menu-item-hover 
+                                  @if (request()->is('dashboard/monitoring')) bg-red-500 text-white @else hover:bg-red-500 @endif 
+                                  rounded-lg p-2">
+                            <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24"
+                                stroke-width="1.5" stroke="currentColor" class="size-6">
+                                <path stroke-linecap="round" stroke-linejoin="round"
+                                    d="M9 17.25v1.007a3 3 0 0 1-.879 2.122L7.5 21h9l-.621-.621A3 3 0 0 1 15 18.257V17.25m6-12V15a2.25 2.25 0 0 1-2.25 2.25H5.25A2.25 2.25 0 0 1 3 15V5.25m18 0A2.25 2.25 0 0 0 18.75 3H5.25A2.25 2.25 0 0 0 3 5.25m18 0V12a2.25 2.25 0 0 1-2.25 2.25H5.25A2.25 2.25 0 0 1 3 12V5.25" />
+                            </svg>
+                            <span class="text-sm">Monitoring</span>
+                        </a>
+                    </li>
                 @endif
             </ul>
         </div>
