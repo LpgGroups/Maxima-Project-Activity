93303cc53821cb83ac59346f01c34d7d0c6ccfc5
muhammadricoasmara04 <ricoasmara491@gmail.com>
2025-07-23 15:18:03 +0700

    update fixing bug interval

--- resources/js/edittrainingadmin.js
+++ resources/js/edittrainingadmin.js
@@ -586,7 +586,6 @@ function timeDeadline() {
                 timeD.textContent = "⛔ Pendaftaran sudah ditutup.";
                 timeD.classList.remove("hidden");
 
-                // Tambahkan style merah ketika waktu habis
                 timeD.classList.remove(
                     "text-blue-600",
                     "bg-white",
@@ -614,7 +613,6 @@ function timeDeadline() {
             timeD.textContent = `⏳ Waktu pendaftaran: ${days}h ${hours}j ${minutes}m ${seconds}d`;
             timeD.classList.remove("hidden");
 
-            // Pastikan style countdown tetap rapi
             timeD.classList.add("text-blue-600", "bg-white", "border-gray-300");
             timeD.classList.remove(
                 "text-red-800",
